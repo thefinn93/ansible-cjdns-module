6a2adbfb270ab5464e5d7f638e16ba6738ef197a
Finn <finn@finn.io>
2015-02-16 02:16:04 -0800

    Initial commit

---
+++ cjdns.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import json
+
+
+def main():
+    module = AnsibleModule(
+        # not checking because of daisy chain to file module
+        argument_spec=dict(
+            authorizedPassword=dict(required=False),
+            cjdroute=dict(required=False, default='/etc/cjdroute.conf'),
+            autoclean=dict(requird=False, type='bool', default=True),
+            udppeer=dict(required=False, type='DictType'),
+            state=dict(default='present', choices=['present', 'absent']),
+        )
+    )
+    changed = False
+    try:
+        import cjdnsadmin
+        params = module.params
+        cjdroute = json.load(open(params['cjdroute']))
+        if params['authorizedPassword'] is not None:
+            if not 'user' in params['authorizedPassword']:
+                module.fail_json(msg='No user specified')
+            elif not 'password' in params['authorizedPassword']:
+                module.fail_json(msg='No password specified')
+            else:
+                position = None
+                for i in range(0, len(cjdroute['authorizedPasswords'])):
+                    if cjdroute['authorizedPasswords'][i]['password'] == params['authorizedPassword']['password'] or cjdroute['authorizedPasswords'][i]['user'] == params['authorizedPassword']['user']:
+                        position = i
+                if params['state'] == 'present':
+                    if position is not None:
+                        if cjdroute['authorizedPasswords'][i]['user'] != params['authorizedPassword']['user']:
+                            cjdroute['authorizedPasswords'][i]['user'] = params['authorizedPassword']['user']
+                            changed = True
+                        if cjdroute['authorizedPasswords'][i]['password'] != params['authorizedPassword']['password']:
+                            cjdroute['authorizedPasswords'][i]['password'] = params['authorizedPassword']['password']
+                            changed = True
+                    else:
+                        cjdroute['authorizedPasswords'].append(params['authorizedPassword'])
+                        changed = True
+                if params['state'] == 'absent' and position is not None:
+                    cjdroute['authorizedPasswords'].pop(i)
+                    changed = True
+        facts = {}
+        if "ipv6" in cjdroute:
+            facts['ip'] = cjdroute['ipv6']
+        if "publicKey" in cjdroute:
+            facts['public_key'] = cjdroute['publicKey']
+        if changed:
+            with open(params['cjdroute'], 'w') as cjdroutefile:
+                json.dump(cjdroute, cjdroutefile, indent=4,
+                          separators=(',', ': '), sort_keys=True)
+        module.exit_json(changed=changed, ansible_facts={"cjdns": facts})
+    except ImportError:
+        module.fail_json(msg='Please install the cjdnsadmin python library')
+    except IOError:
+        module.fail_json(msg='Failed to open cjdroute.conf')
+
+# import module snippets
+from ansible.module_utils.basic import *
+main()
